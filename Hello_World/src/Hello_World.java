bab77de6bf0597602af8ef208b2edcfcda8a85f6
APCS2nd <APCS2nd@MHS-Dreyer-15>
2018-08-21 11:43:35 -0700

    :))))))))))

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -1,4 +1,14 @@
-
-public class Hello_World {
-
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (Nicole Leon) 
+ * @version (August 2018)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <your name>"); //Display the string
+    }
 }
+
